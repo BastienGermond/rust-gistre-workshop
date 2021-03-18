64525fd99e14042464c81ad5b562bc00858174f0
Bastien Germond <bastien.germond@epita.fr>
2021-03-18 21:45:24 +0100

    finish: cat

--- 0_intro/exercises/cat/src/main.rs
+++ 0_intro/exercises/cat/src/main.rs
@@ -1,7 +1,14 @@
+use std::env;
+use std::io::Read;
+
 pub fn cat(path: &str) -> String {
-    todo!();
+    let mut file = std::fs::File::open(path).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    contents
 }
 
 fn main() {
-    todo!();
+    let args: Vec<String> = env::args().collect();
+    println!("{}", cat(&args[1]));
 }
