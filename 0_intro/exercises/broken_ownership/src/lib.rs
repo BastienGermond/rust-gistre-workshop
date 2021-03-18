ccac097f26ed8fba87bdf82d0f296e8e9595ed08
Bastien Germond <bastien.germond@epita.fr>
2021-03-18 21:17:48 +0100

    finish: broken_ownership
    borrow checker is quite mean

--- 0_intro/exercises/broken_ownership/src/lib.rs
+++ 0_intro/exercises/broken_ownership/src/lib.rs
@@ -1,27 +1,25 @@
 /// Create vector and fill it, then return it
 /// Help me `modify_values` won't give my vector back :(
-// FIXME
 pub fn get_answer() -> Vec<i32> {
     let mut values = vec![0, 0];
-    modify_values(values);
+    modify_values(&mut values);
     values
 }
 
-// FIXME
-fn modify_values(values: Vec<i32>) {
+fn modify_values(values: &mut Vec<i32>) {
     values[0] = 4;
     values[1] = 2;
 }
 
 /// Print the first word of the "hello world" message, then free the message
 /// Help me the borrow checker is bullying me :(
-// FIXME
 pub fn print_hello() {
     let mut msg = String::from("hello world");
+
     let word = first_word(&msg); // Get a reference to the first word
+    println!("The first word is: {}", word); // Print first word
 
     msg.clear(); // Empty the message
-    println!("The first word is: {}", word); // Print first word
 }
 
 fn first_word(msg: &String) -> &str {
